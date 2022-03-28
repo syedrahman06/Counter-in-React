63eb44aba36be309adfc5d1ab29bc65b0df1898c
Syed Abdur Rahman Uddin <57063763+syedrahman24@users.noreply.github.com>
2022-03-29 02:52:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Custom from './components/custom';
+import RandomQuote from './components/RandomQuote';
+
+function App() {
+  return (
+    <>
+
+      <Custom />
+
+
+
+
+    </>
+
+  );
+}
+
+export default App;
