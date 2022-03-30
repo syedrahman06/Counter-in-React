1d5e7cf5eacbe5e106340df8da61f5f2c07b328f
Syed Abdur Rahman Uddin <57063763+syedrahman24@users.noreply.github.com>
2022-03-30 16:08:31 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-import Custom from './components/custom';
-import RandomQuote from './components/RandomQuote';
+import { useState, useRef } from 'react';
+
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [currentTask, setCurrentTask] = useState("");
+  const inputTask = useRef(null);
+
+  const addTask = () => {
+    setTodoList([...todoList, currentTask]);
+    inputTask.current.value = "";
+    setCurrentTask("");
+
+  }
+
   return (
-    <>
+    <div className="App">
+      <div className='head'>
 
-      <Custom />
+        <h1>To Do List App</h1>
+        <div className='task-head'>
+          <input
+            type="text"
+            placeholder='Task...'
+            ref={inputTask}
+            onChange={(event) => {
+              setCurrentTask(event.target.value)
+            }} />
 
+          <button onClick={addTask}>Add Task</button>
+        </div>
 
+      </div>
 
+      <hr />
 
-    </>
+      <div>
+        <ul>
+          {todoList.map((val, key) => {
+            return <li key={key}>{val}</li>
+          })}
+        </ul>
+      </div>
+
+    </div >
 
   );
 }
